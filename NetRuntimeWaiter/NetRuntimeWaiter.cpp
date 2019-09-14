e6d255a6638a347369c4541768fe06d8e1587658
Friedrich von Never <friedrich@fornever.me>
2019-09-14 18:24:30 +0700

    Use NtDebugActiveProcess

--- NetRuntimeWaiter/NetRuntimeWaiter.cpp
+++ NetRuntimeWaiter/NetRuntimeWaiter.cpp
@@ -3,6 +3,138 @@
 
 #include <Windows.h>
 
+
+#include <winternl.h>
+
+
+extern "C"
+NTSYSCALLAPI NTSTATUS NTAPI NtWaitForDebugEvent	(	_In_ HANDLE 	DebugObjectHandle,
+_In_ BOOLEAN 	Alertable,
+_In_opt_ PLARGE_INTEGER 	Timeout,
+_Out_ PVOID 	WaitStateChange
+);
+
+#define DEBUG_READ_EVENT 0x0001
+#define DEBUG_PROCESS_ASSIGN 0x0002
+#define DEBUG_SET_INFORMATION 0x0004
+#define DEBUG_QUERY_INFORMATION 0x0008
+#define DEBUG_ALL_ACCESS (STANDARD_RIGHTS_REQUIRED | SYNCHRONIZE | \
+    DEBUG_READ_EVENT | DEBUG_PROCESS_ASSIGN | DEBUG_SET_INFORMATION | \
+    DEBUG_QUERY_INFORMATION)
+
+#pragma comment (linker, "/defaultlib:ntdll.lib")
+
+extern "C"
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtDebugActiveProcess(
+    _In_ HANDLE ProcessHandle,
+    _In_ HANDLE DebugObjectHandle
+    );
+
+extern "C"
+NTSYSAPI
+NTSTATUS
+NTAPI
+NtCreateDebugObject(
+  OUT PHANDLE             DebugObjectHandle,
+  IN ACCESS_MASK          DesiredAccess,
+  IN POBJECT_ATTRIBUTES   ObjectAttributes OPTIONAL,
+  IN BOOLEAN              KillProcessOnExit );
+
+ //
+ // Debug States
+ //
+ typedef enum _DBG_STATE
+ {
+     DbgIdle,
+     DbgReplyPending,
+     DbgCreateThreadStateChange,
+     DbgCreateProcessStateChange,
+     DbgExitThreadStateChange,
+     DbgExitProcessStateChange,
+     DbgExceptionStateChange,
+     DbgBreakpointStateChange,
+     DbgSingleStepStateChange,
+     DbgLoadDllStateChange,
+     DbgUnloadDllStateChange
+ } DBG_STATE, *PDBG_STATE;
+
+ //
+ // Debug Message Structures
+ //
+ typedef struct _DBGKM_EXCEPTION
+ {
+     EXCEPTION_RECORD ExceptionRecord;
+     ULONG FirstChance;
+ } DBGKM_EXCEPTION, *PDBGKM_EXCEPTION;
+
+ typedef struct _DBGKM_CREATE_THREAD
+ {
+     ULONG SubSystemKey;
+     PVOID StartAddress;
+ } DBGKM_CREATE_THREAD, *PDBGKM_CREATE_THREAD;
+
+ typedef struct _DBGKM_CREATE_PROCESS
+ {
+     ULONG SubSystemKey;
+     HANDLE FileHandle;
+     PVOID BaseOfImage;
+     ULONG DebugInfoFileOffset;
+     ULONG DebugInfoSize;
+     DBGKM_CREATE_THREAD InitialThread;
+ } DBGKM_CREATE_PROCESS, *PDBGKM_CREATE_PROCESS;
+
+ typedef struct _DBGKM_EXIT_THREAD
+ {
+     NTSTATUS ExitStatus;
+ } DBGKM_EXIT_THREAD, *PDBGKM_EXIT_THREAD;
+
+ typedef struct _DBGKM_EXIT_PROCESS
+ {
+     NTSTATUS ExitStatus;
+ } DBGKM_EXIT_PROCESS, *PDBGKM_EXIT_PROCESS;
+
+ typedef struct _DBGKM_LOAD_DLL
+ {
+     HANDLE FileHandle;
+     PVOID BaseOfDll;
+     ULONG DebugInfoFileOffset;
+     ULONG DebugInfoSize;
+     PVOID NamePointer;
+ } DBGKM_LOAD_DLL, *PDBGKM_LOAD_DLL;
+
+ typedef struct _DBGKM_UNLOAD_DLL
+ {
+     PVOID BaseAddress;
+ } DBGKM_UNLOAD_DLL, *PDBGKM_UNLOAD_DLL;
+
+typedef struct _DBGUI_WAIT_STATE_CHANGE
+ {
+     DBG_STATE NewState;
+     CLIENT_ID AppClientId;
+     union
+     {
+         struct
+         {
+             HANDLE HandleToThread;
+             DBGKM_CREATE_THREAD NewThread;
+         } CreateThread;
+         struct
+         {
+             HANDLE HandleToProcess;
+             HANDLE HandleToThread;
+             DBGKM_CREATE_PROCESS NewProcess;
+         } CreateProcessInfo;
+         DBGKM_EXIT_THREAD ExitThread;
+         DBGKM_EXIT_PROCESS ExitProcess;
+         DBGKM_EXCEPTION Exception;
+         DBGKM_LOAD_DLL LoadDll;
+         DBGKM_UNLOAD_DLL UnloadDll;
+     } StateInfo;
+ } DBGUI_WAIT_STATE_CHANGE, *PDBGUI_WAIT_STATE_CHANGE;
+
 constexpr auto processPath = L"C:\\Windows\\SYSWOW64\\notepad.exe";
 PROCESS_INFORMATION startProcessSuspended()
 {
@@ -34,30 +166,33 @@ void terminateProcess(PROCESS_INFORMATION processInformation)
 	std::cout << "Terminate process " << processInformation.dwProcessId << (result ? " success" : " error") << std::endl;
 }
 
-bool debugProcessLoop()
+extern "C"
+NTSYSCALLAPI NTSTATUS NTAPI 	NtDebugContinue (_In_ HANDLE DebugObjectHandle, _In_ CLIENT_ID* ClientId, _In_ NTSTATUS ContinueStatus);
+
+bool debugProcessLoop(HANDLE debugObject)
 {
-	DEBUG_EVENT debugEvent = {};
+	// DEBUG_EVENT debugEvent = {};
 	auto firstThreadCreated = false;
-	while (WaitForDebugEvent(&debugEvent, INFINITE))
+	DBGUI_WAIT_STATE_CHANGE debugEvent = {};
+	LARGE_INTEGER timeout = {};
+	timeout.LowPart = 60000;
+	NTSTATUS waitStatus;
+	while ((waitStatus = NtWaitForDebugEvent(debugObject, true, nullptr, &debugEvent)) == 0)
 	{
-		std::cout << "DebugEvent: " << debugEvent.dwDebugEventCode << " (thread: " << debugEvent.dwThreadId << ")" << std::endl;
-		switch (debugEvent.dwDebugEventCode)
+		std::cout << "DebugEvent: " << debugEvent.NewState << std::endl;
+		switch (debugEvent.NewState)
 		{
-		case OUTPUT_DEBUG_STRING_EVENT:
-			std::cout << "  OUTPUT_DEBUG_STRING_EVENT" << std::endl;
-			break;
-		case UNLOAD_DLL_DEBUG_EVENT:
-			std::cout << "  UNLOAD_DLL_DEBUG_EVENT" << std::endl;
+		case DbgUnloadDllStateChange:
+			std::cout << "  DbgUnloadDllStateChange" << std::endl;
 	        break;
-		case CREATE_PROCESS_DEBUG_EVENT:
-			std::cout << "  CREATE_PROCESS_DEBUG_EVENT: "
-				<< "ProcessId = " << GetProcessId(debugEvent.u.CreateProcessInfo.hProcess)
-				<< " ThreadId = " << GetThreadId(debugEvent.u.CreateProcessInfo.hThread) << std::endl;
-			CloseHandle(debugEvent.u.CreateProcessInfo.hFile);
+		case DbgCreateProcessStateChange:
+			std::cout << "  DbgCreateProcessStateChange: "
+				<< "ProcessId = " << GetProcessId(debugEvent.StateInfo.CreateProcessInfo.HandleToProcess)
+				<< " ThreadId = " << GetThreadId(debugEvent.StateInfo.CreateProcessInfo.HandleToThread) << std::endl;
 			break;
-		case EXCEPTION_DEBUG_EVENT: {
-			auto exceptionCode = debugEvent.u.Exception.ExceptionRecord.ExceptionCode;
-			std::cout << "  EXCEPTION: " << exceptionCode << std::endl;
+		case DbgExceptionStateChange: {
+			auto exceptionCode = debugEvent.StateInfo.Exception.ExceptionRecord.ExceptionCode;
+			std::cout << "  DbgExceptionStateChange: " << exceptionCode << std::endl;
 			if (exceptionCode == EXCEPTION_ACCESS_VIOLATION)
 			{
 				std::cout << "    This is EXCEPTION_ACCESS_VIOLATION" << std::endl;
@@ -65,40 +200,49 @@ bool debugProcessLoop()
 			}
 			break;
 		}
-		case CREATE_THREAD_DEBUG_EVENT:
-			std::cout << "  CREATE_THREAD_DEBUG_EVENT: " << GetThreadId(debugEvent.u.CreateThread.hThread) << std::endl;
+		case DbgCreateThreadStateChange:
+			std::cout << "  DbgCreateThreadStateChange: " << GetThreadId(debugEvent.StateInfo.CreateThread.HandleToThread) << std::endl;
 			firstThreadCreated = true;
 			break;
-		case LOAD_DLL_DEBUG_EVENT:
-			std::cout << "  LOAD_DLL_DEBUG_EVENT" << std::endl;
-			CloseHandle(debugEvent.u.LoadDll.hFile);
+		case DbgLoadDllStateChange:
+			std::cout << "  DbgLoadDllStateChange" << std::endl;
+			CloseHandle(debugEvent.StateInfo.LoadDll.FileHandle);
 			if (firstThreadCreated)
 				return true; // success
 			break;
-		case EXIT_PROCESS_DEBUG_EVENT:
-			std::cout << "  EXIT_PROCESS_DEBUG_EVENT" << std::endl;
+		case DbgExitProcessStateChange:
+			std::cout << "  DbgExitProcessStateChange" << std::endl;
 			// to close all the handles
-			ContinueDebugEvent(debugEvent.dwProcessId, debugEvent.dwThreadId, DBG_CONTINUE);
+
+			// ContinueDebugEvent(debugEvent.dwProcessId, debugEvent.dwThreadId, DBG_CONTINUE);
 			return true;
 		default:
 			std::cout << "  (UNKNOWN)" << std::endl;
 			break;
 		}
 
-		const auto result = ContinueDebugEvent(debugEvent.dwProcessId, debugEvent.dwThreadId, DBG_EXCEPTION_NOT_HANDLED);
-		if (!result)
+		const auto result = NtDebugContinue(debugObject, &debugEvent.AppClientId, DBG_EXCEPTION_NOT_HANDLED);
+		if (result != 0)
 		{
-			std::cout << "ContinueDebugEvent error" << std::endl;
+			std::cout << "NtDebugContinue error: " << result << std::endl;
 			return false;
 		}
 	}
 
+	std::cout << "NtWaitForDebugEvent returned error code: " << waitStatus << std::endl;
 	return false;
 }
 
 bool debugProcess(PROCESS_INFORMATION processInformation)
 {
-	if (!DebugActiveProcess(processInformation.dwProcessId))
+	OBJECT_ATTRIBUTES attributes;
+	InitializeObjectAttributes(&attributes, 0, 0, 0, 0);
+
+	HANDLE debugObjectHandle = nullptr;
+	auto createResult = NtCreateDebugObject(&debugObjectHandle, DEBUG_ALL_ACCESS, &attributes, false);
+	std::cout << "NtCreateDebugObject result: " << createResult << std::endl;
+
+	if (NtDebugActiveProcess(processInformation.hProcess, debugObjectHandle) != 0)
 	{
 		std::cout << "DebugActiveProcess error: " << GetLastError() << std::endl;
 		return false;
@@ -110,7 +254,7 @@ bool debugProcess(PROCESS_INFORMATION processInformation)
 		return false;
 	}
 
-	const auto result = debugProcessLoop();
+	const auto result = debugProcessLoop(debugObjectHandle);
 	if (!DebugActiveProcessStop(processInformation.dwProcessId))
 	{
 		std::cout << "DebugActiveProcessStop error: " << GetLastError() << std::endl;
